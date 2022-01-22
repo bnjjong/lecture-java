5dffcb06de849d93f44cbcc60aa7327473441d6b
Jordan <jongsang@bigin.io>
2022-01-22 16:21:18 +0900

    Initial commit

---
+++ src/main/java/org/jjong/lecture/day1/Hello.java
@@ -0,0 +1,28 @@
+/*
+ * Created By dogfootmaster@gmail.com on 2022
+ * This program is free software
+ *
+ * @author <a href=“mailto:dogfootmaster@gmail.com“>Jongsang Han</a>
+ * @since 2022/01/22
+ */
+
+package org.jjong.lecture.day1;
+
+/**
+ * create on 2022/01/22. create by IntelliJ IDEA.
+ *
+ * <p> 클래스 설명 </p>
+ * <p> {@link } and {@link }관련 클래스 </p>
+ *
+ * @author Jongsang Han
+ * @version 1.0
+ * @see
+ * @since 1.0
+ */
+public class Hello {
+
+  public static void main(String[] args) {
+    System.out.println("hello world!");
+  }
+
+}
